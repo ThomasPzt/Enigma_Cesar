a7d5a7f9cbd0c0b83fd32134f13d4b3f5403e979
esteban-brion <estebanbrion@gmail.com>
2024-01-29 11:47:48 -0500

    chiffrage avec gestion des accents, de la ponctuation

--- chiffrage.py
+++ chiffrage.py
@@ -1,34 +1,33 @@
+from unidecode import unidecode
+
 def caesar_cipher(text, key):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     result = ''
     for char in text:
         if char.isalpha():
-            is_upper = char.isupper()
-            char_lower = char.lower()
-            index = (ord(char_lower) - ord('a') + key) % 26
+            index = (ord(char) + key) % 26
             encrypted_char = alphabet[index]
-            result += encrypted_char.upper() if is_upper else encrypted_char
+            result += encrypted_char
         else:
             result += char
     return result
-
 def encrypt_decrypt_file(file_path, key, encrypt=True):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
 
-    # Convertir le texte en minuscules tout en conservant la ponctuation
-    content_lower = ''.join([char.lower() if char.isalpha() else char for char in content])
+    # Convertir le texte en minuscules tout en conservant la ponctuation et gérer les accents
+    content_normalized = unidecode(''.join([char.lower() if char.isalpha() else char for char in content]))
 
     # Ajuster la clé en fonction de l'option choisie (chiffrer ou déchiffrer)
     key = key if encrypt else -key
 
     # Appliquer le chiffrement César avec la clé spécifiée
-    encrypted_text = caesar_cipher(content_lower, key)
+    encrypted_text = caesar_cipher(content_normalized, key)
 
     # Écrire le texte chiffré/déchiffré dans un nouveau fichier
     operation = 'Chiffré' if encrypt else 'Déchiffré'
     output_file_path = f'{operation}_output.txt'
-    with open(output_file_path, 'w') as output_file:
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
         output_file.write(encrypted_text)
 
     print(f"Texte {operation.lower()} avec la clé {key}. Résultat enregistré dans {output_file_path}.")
